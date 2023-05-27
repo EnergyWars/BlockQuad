97940521df8115d46d1a090c1a092a122112ffb6
Simon Klein <you@example.com>
2023-05-27 08:21:26 +0200

    Update

--- Assets/MeshUtils.cs
+++ Assets/MeshUtils.cs
@@ -27,48 +27,18 @@ public static class MeshUtils
 
     public static Vector2[,] blockUVs =
     {
-        /* GRASSTOP */
-        {
-            new Vector2(0.125f, 0.375f),
-            new Vector2(0.1875f, 0.375f),
-            new Vector2(0.125f, 0.4375f),
-            new Vector2(0.1875f, 0.475f)
-        },
-        /* GRASSSIDE */
-        {
-            new Vector2(0.1875f, 0.9375f),
-            new Vector2(0.25f, 0.9375f),
-            new Vector2(0.1875f, 1.0f),
-            new Vector2(0.25f, 1.0f)
-        },
-        /* DIRT */
-        {
-            new Vector2(0.125f,0.9375f),
-            new Vector2(0.1875f,0.9375f),
-            new Vector2(0.125f, 1.0f),
-            new Vector2(0.1875f, 1.0f),
-        },
-        /* WATER */
-        {
-            new Vector2(0.875f,0.125f),
-            new Vector2(0.9375f,0.125f),
-            new Vector2(0.875f,0.1875f),
-            new Vector2(0.9375f,0.1875f),
-        },
-        /* STONE */
-        {
-            new Vector2(0,0.875f),
-            new Vector2(0.0625f,0.875f),
-            new Vector2(0,0.9375f),
-            new Vector2(0.0625f,0.9375f),
-        },
-        /* SAND */
-        {
-            new Vector2(0.125f,0.875f),
-            new Vector2(0.1875f,0.875f),
-            new Vector2(0.125f,0.9375f),
-            new Vector2(0.1875f,0.9375f),
-        },
+        /*GRASSTOP*/ {  new Vector2(0.125f, 0.375f), new Vector2(0.1875f,0.375f),
+            new Vector2(0.125f, 0.4375f), new Vector2(0.1875f,0.4375f) },
+        /*GRASSSIDE*/ { new Vector2( 0.1875f, 0.9375f ), new Vector2( 0.25f, 0.9375f),
+            new Vector2( 0.1875f, 1.0f ),new Vector2( 0.25f, 1.0f )},
+        /*DIRT*/	  { new Vector2( 0.125f, 0.9375f ), new Vector2( 0.1875f, 0.9375f),
+            new Vector2( 0.125f, 1.0f ),new Vector2( 0.1875f, 1.0f )},
+        /*WATER*/	  { new Vector2(0.875f,0.125f),  new Vector2(0.9375f,0.125f),
+            new Vector2(0.875f,0.1875f), new Vector2(0.9375f,0.1875f)},
+        /*STONE*/	  { new Vector2( 0, 0.875f ), new Vector2( 0.0625f, 0.875f),
+            new Vector2( 0, 0.9375f ),new Vector2( 0.0625f, 0.9375f )},
+        /*SAND*/	  { new Vector2(0.125f,0.875f),  new Vector2(0.1875f,0.875f),
+            new Vector2(0.125f,0.9375f), new Vector2(0.1875f,0.9375f)}
     };
 
 
